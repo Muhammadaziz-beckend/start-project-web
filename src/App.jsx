940190f7fb263fc9851d01413442837bdb3369fe
Muhammadaziz <muhammadazizdev@gmail.com>
2025-06-05 19:06:47 +0600

    add Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import "./static/css/style.css";
 
 const App = () => {
-  return <>hello</>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<>hello</>} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
