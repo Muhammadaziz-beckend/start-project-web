232683d33ebc828d6182df03f9f0275a6d470154
Muhammadaziz <muhammadazizdev@gmail.com>
2026-06-25 14:51:17 +0600

    update utils

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: [''],
+    host: "0.0.0.0",  // Access from any device in the local network
+    port: 5174,        // You can change the port if needed
+    mimeTypes: {
+      'application/javascript': ['js'],
+    },
+  },
   plugins: [react()],
+  base: "/",           // Update this if deploying to a subdirectory
+  build: {
+    chunkSizeWarningLimit: 1000,  // Adjust chunk size warning limit (optional)
+  },
 })
