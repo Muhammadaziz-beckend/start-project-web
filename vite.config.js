395ebe61262e558e5a14b1aed8994be0bbc3aed8
Muhammadaziz <muhammadazizdev@gmail.com>
2026-09-03 16:38:04 +0600

    add

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    allowedHosts: [''],
+    allowedHosts: ['offis.maximumcomfort.pro'],
     host: "0.0.0.0",  // Access from any device in the local network
     port: 5174,        // You can change the port if needed
     mimeTypes: {
